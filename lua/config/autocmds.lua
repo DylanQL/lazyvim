1ed9f2ed6f67e9f77872b75bffbb9a507ef1a4be
Angelo Dylan <angelo.quispe.l@tecsup.edu.pe>
2025-07-30 13:03:19 -0500

    Añadiendo metodo de identación en autocmd para los HTML

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,3 +6,19 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "html",
+  callback = function()
+    -- No formatear automáticamente al guardar
+    vim.b.autoformat = false
+
+    -- Configura indentación a 4 espacios
+    vim.opt_local.shiftwidth = 4
+    vim.opt_local.tabstop = 4
+    vim.opt_local.softtabstop = 4
+    vim.opt_local.expandtab = true
+    vim.opt_local.autoindent = true
+    vim.opt_local.smartindent = true
+  end,
+})
