71c5fae199d5dc5c67b2637cd773962b0538ec6f
Angelo Dylan <angelo.quispe.l@tecsup.edu.pe>
2025-12-16 00:56:12 -0500

    Feat: Configurar el colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,15 +1,18 @@
 return {
-  { "catppuccin/nvim", name = "catppuccin", lazy = false, opts = { flavour = "mocha" } },
+  -- add oxocarbon
   {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "oxocarbon",
-    },
-  },
+    "nyoom-engineering/oxocarbon.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.cmd.colorscheme("oxocarbon")
+      vim.api.nvim_set_hl(0, "NormalFloat", { bg = "NONE" })
+      vim.api.nvim_set_hl(0, "FloatBorder", { fg = "#5f5f87" })
+      -- comentarios gris claro
+      vim.api.nvim_set_hl(0, "Comment", { fg = "#7f7f87" })
 
-  "nyoom-engineering/oxocarbon.nvim",
-  -- Add in any other configuration;
-  --   event = foo,
-  --   config = bar
-  --   end,
+      -- configurando visual selection con un color de fondo personalizado y letras blancas
+      vim.api.nvim_set_hl(0, "Visual", { bg = "#6c007f", fg = "#ffffff" })
+    end,
+  },
 }
